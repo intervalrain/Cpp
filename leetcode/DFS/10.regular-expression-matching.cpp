92ecae63d4bbc5b1496c643d4dccd753f69a6c4f
Rain Hu <intervalrain@gmail.com>
2022-08-16 23:29:49 +0800

    update leetcode no.10

---
+++ leetcode/DFS/10.regular-expression-matching.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=10 lang=cpp
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.length() == 0) return s.length() == 0;
+        bool firstMatch = s.length() > 0 && (s[0] == p[0] || p[0] == '.');
+
+        if (p.length() >= 2 && p[1] == '*'){
+            return isMatch(s, p.substr(2)) || (firstMatch && isMatch(s.substr(1), p));
+        }
+        return firstMatch && isMatch(s.substr(1), p.substr(1));
+    }
+};
+// @lc code=end
--- leetcode/DP/10.regular-expression-matching.cpp
+++ leetcode/DP/10.regular-expression-matching.cpp
@@ -7,17 +7,6 @@
 // @lc code=start
 class Solution {
 public:
-    // dfs
-    // bool isMatch(string s, string p) {
-    //     if (p.length() == 0) return s.length() == 0;
-    //     bool firstMatch = s.length() > 0 && (s[0] == p[0] || p[0] == '.');
-
-    //     if (p.length() >= 2 && p[1] == '*'){
-    //         return isMatch(s, p.substr(2)) || (firstMatch && isMatch(s.substr(1), p));
-    //     }
-    //     return firstMatch && isMatch(s.substr(1), p.substr(1));
-    // }
-    // dp
     bool isMatch(string s, string p){
         int m = s.length(), n = p.length();
         if (n == 0) return m == 0;
