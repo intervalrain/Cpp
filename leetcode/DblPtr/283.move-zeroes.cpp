3de61a98144c9371c0e729bd2e7ee9e587ed50f4
Rain Hu <intervalrain@gmail.com>
2022-08-16 23:49:29 +0800

    update leedcode no.283

---
+++ leetcode/DblPtr/283.move-zeroes.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != 0){
+                nums[pos++] = nums[i];
+            }
+        }
+        for (int i = pos; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+    }
+};
+// @lc code=end
+
