82c9d5802f72561538fb5ef3277b07aeea53e78a
Rain Hu <intervalrain@gmail.com>
2022-08-19 01:47:37 +0800

    update leetcode no.16

---
+++ leetcode/DblPtr/16.3-sum-closest.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=16 lang=cpp
+ *
+ * [16] 3Sum Closest
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int res = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.size()-2; i++){
+            int j = i+1, k = nums.size()-1;
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target){
+                    if (abs(sum-target) < abs(res-target)) res = sum;
+                    j++;
+                } else if (sum > target){
+                    if (abs(sum-target) < abs(res-target)) res = sum;
+                    k--;
+                } else {
+                    return target;
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
