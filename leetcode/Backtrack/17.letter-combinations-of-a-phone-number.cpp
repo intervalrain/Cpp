84e937eb763582d19ab229aa12736b6d9682ff62
Rain Hu <intervalrain@gmail.com>
2022-08-19 02:59:17 +0800

    update leetcode no.17

---
+++ leetcode/Backtrack/17.letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=17 lang=cpp
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.length() == 0) return {};
+        vector<string> map = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> res;
+        string path;
+        backtrack(digits, map, res, path, 0);
+        return res;
+    }
+    void backtrack(string& digits, vector<string>& map, vector<string>& res, string& path, int i){
+        if (i == digits.length()){
+            res.push_back(path);
+            return;
+        }
+        char c = digits[i];
+        string str = map[c-'2'];
+        for (char x : str){
+            path.push_back(x);
+            backtrack(digits, map, res, path, i+1);
+            path.pop_back();
+        }
+    }
+};
+// @lc code=end
