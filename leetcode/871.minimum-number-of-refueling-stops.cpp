1359644756f10ea1365e95500cf28dd3d01f4883
Rain Hu <intervalrain@gmail.com>
2022-08-21 03:32:02 +0800

    update leetcode no.871

---
+++ leetcode/871.minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=871 lang=cpp
+ *
+ * [871] Minimum Number of Refueling Stops
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        if (startFuel >= target) return 0;
+        if (startFuel < target && stations.size() == 0);
+        priority_queue<int> pq;
+        pq.push(startFuel);
+        int cnt = 0;
+        int reach = 0;
+        int i = 0;
+        while (!pq.empty()){
+            reach += pq.top();
+            pq.pop();
+            if (reach >= target) return cnt;
+            for (; i < stations.size(); i++){
+                if (reach < stations[i][0]) break;
+                pq.push(stations[i][1]);
+            }
+            cnt++;
+
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
