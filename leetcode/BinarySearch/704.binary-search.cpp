a3cd478799959fe835c7cdacb0ec8766a19b4621
Rain Hu <intervalrain@gmail.com>
2022-08-22 00:10:42 +0800

    update leetcode no.704

---
+++ leetcode/BinarySearch/704.binary-search.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int& target) {
+        ios::sync_with_stdio(false);
+        int left = 0, right = nums.size()-1;
+        while (left <= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
