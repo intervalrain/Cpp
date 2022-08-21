d1b5e8f6b724e26963d8a551597c91cb7959a203
Rain Hu <intervalrain@gmail.com>
2022-08-22 01:25:24 +0800

    update leetcode no.438

---
+++ leetcode/SlidingWindow/438.find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=438 lang=cpp
+ *
+ * [438] Find All Anagrams in a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int window[26] = {0};
+        int need[26] = {0};
+        int left = 0, right = 0;
+        int len = 0;
+        int valid = 0;
+        int n = s.length();
+        vector<int> res;
+        for (char c : p){
+            int& cnt = need[c-'a'];
+            cnt++;
+            if (cnt == 1) len++;
+        } 
+        while (right < n){
+            char& c = s[right];
+            right++;
+            if (need[c-'a'] > 0){
+                window[c-'a']++;
+                if (window[c-'a'] == need[c-'a']) valid++;
+            }
+            if (right - left == p.length()){
+                if (valid == len) res.push_back(left);
+                char& d = s[left];
+                left++;
+                if (need[d-'a'] > 0){
+                    if (window[d-'a'] == need[d-'a']) valid--;
+                    window[d-'a']--;
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
