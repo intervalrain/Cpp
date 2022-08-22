2600bcbdae516dfb43f020277daec1dd0743be00
Rain Hu <intervalrain@gmail.com>
2022-08-23 00:29:37 +0800

    update leetcode no.342

---
+++ leetcode/342.power-of-four.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=342 lang=cpp
+ *
+ * [342] Power of Four
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        if (n & (n-1)) return false;
+        if ((0x55555555 & n) != n) return false;
+        return true;
+    }
+};
+// @lc code=end
+
+
