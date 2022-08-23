869feb5c55dff6aaa3588279ff7536b0f7ab3f14
Rain Hu <intervalrain@gmail.com>
2022-08-23 20:55:47 +0800

    update leetcode no.55

---
+++ leetcode/array/55.jump-game.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = nums[0];
+        for (int i = 1; i <= reach && i < nums.size(); i++){
+            reach = max(reach, nums[i] + i);
+        }
+        return reach >= nums.size()-1;
+    }
+};
+// @lc code=end
+
