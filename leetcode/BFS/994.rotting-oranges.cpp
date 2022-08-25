fa682e7d52af40613328848d92b23c885ae43d3c
Rain Hu <intervalrain@gmail.com>
2022-08-25 21:35:51 +0800

    update leetcode no.994

---
+++ leetcode/BFS/994.rotting-oranges.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=994 lang=cpp
+ *
+ * [994] Rotting Oranges
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> dirc = {{1,0},{-1,0},{0,1},{0,-1}};
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int,int>> q;
+        int depth = -1;
+        int cnt = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 2){
+                    q.push({i, j});
+                } else if (grid[i][j] == 1){
+                    cnt++;
+                }
+            }
+        }
+        if (cnt == 0) return 0;
+        while (!q.empty()){
+            int sz = q.size();
+            for (int i = 0; i < sz; i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for (auto d : dirc){
+                    if (isValid(grid, row+d[0], col+d[1])){
+                        cnt--;
+                        grid[row+d[0]][col+d[1]] = 2;
+                        q.push({row+d[0], col+d[1]});
+                    }
+                }
+            }
+            depth++;
+        }
+        return cnt == 0 ? depth : -1;
+    }
+    bool isValid(vector<vector<int>>& grid, int row, int col){
+        return row >= 0 && col >= 0 && row < grid.size() && col < grid[0].size() && grid[row][col] == 1;
+    }
+};
+// @lc code=end
+
