a3c9cca075f896e02745ddbb1951ae0a958587f9
Rain Hu <intervalrain@gmail.com>
2022-08-27 01:31:41 +0800

    update leetcode no.35

---
+++ leetcode/35.search-insert-position.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        while (left <= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
+// @lc code=end
+
