f4e53fbf6f7242b4efd3bfd50360ff2df2c84585
Rain Hu <intervalrain@gmail.com>
2022-09-02 01:58:11 +0800

    update leetcode no.492

---
+++ leetcode/Math/492.construct-the-rectangle.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=492 lang=cpp
+ *
+ * [492] Construct the Rectangle
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int sq = sqrt(area);
+        
+        for (int i = sq; i <= area; i++){
+            int div = area/i;
+            if (div * i == area && div <= i) return {i, div};
+        }
+        return {area, 1};
+    }
+};
+// @lc code=end
+
