e5193073962388a022fc5ca89baab4cdbe647a34
Rain Hu <intervalrain@gmail.com>
2022-09-06 15:30:16 +0800

    update leetcode no.814

---
+++ leetcode/BinaryTree/814.binary-tree-pruning.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+ * @lc app=leetcode id=814 lang=cpp
+ *
+ * [814] Binary Tree Pruning
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (!root->left && !root->right && root->val == 0) return NULL;
+        return root;
+    }
+};
+// @lc code=end
+
