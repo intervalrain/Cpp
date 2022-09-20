4c6f9229efc69057f8bdc8edcf16f87ee0c008ab
Rain Hu <intervalrain@gmail.com>
2022-09-20 15:37:02 +0800

    update leetcode no.140

---
+++ leetcode/140.word-break-ii.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+ * @lc app=leetcode id=140 lang=cpp
+ *
+ * [140] Word Break II
+ */
+
+// @lc code=start
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> res;
+        string path;
+        helper(s, wordDict, res, path);
+        return res;
+    }
+    void helper(string s, vector<string>& wordDict, vector<string>& res, string path){
+        if (s.length() == 0){
+            res.push_back(path.substr(0, path.length()-1));
+        }
+        for (string& word : wordDict){
+            if (isPrefix(s, word)){
+                path += (word + " ");
+                helper(s.substr(word.length()), wordDict, res, path);
+                path = path.substr(0, path.length() - 1 - word.length());
+            }
+        }
+    }
+    
+    bool isPrefix(string s, string t){
+        if (t.length() > s.length()) return false;
+        return s.substr(0, t.length()) == t;
+    }
+// @lc code=end
+
