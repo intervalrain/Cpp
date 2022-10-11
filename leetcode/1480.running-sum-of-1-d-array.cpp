ea904e54c9da1954e08a19713cddb94e562625e9
Rain Hu <intervalrain@gmail.com>
2022-10-11 20:57:59 +0800

    update leetcode no. 1480

---
+++ leetcode/1480.running-sum-of-1-d-array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+ * @lc app=leetcode id=1480 lang=cpp
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+};
+// @lc code=end
+
